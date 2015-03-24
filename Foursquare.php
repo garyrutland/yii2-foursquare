59e5d7822daba19be45b428f26a4422b8865b6bb
Gary Rutland <gary.rutland@miniclip.com>
2015-03-24 08:34:46 +0000

    Initial commit

---
+++ Foursquare.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace garyrutland\foursquare;
+
+use yii\base\Component;
+
+class Foursquare extends Component
+{
+    public function init()
+    {
+        parent::init();
+    }
+
+
+}
